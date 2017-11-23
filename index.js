39551c7f34e9c988b364c8b633e6ae98227ecb60
Krister Johansson <krister.johansson86@outlook.com>
2017-11-23 21:08:05 +0100

    domp

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const moment = require('moment')
-
+const dotenv = require('dotenv').config()
 const app = express()
 
 const PORT = process.env.PORT || 3000
+const FORMAT = process.env.FORMAT || 'YYYYMMDD'
 
 app.get('/', (req, res) => {
     res.json({
-        redirect_to_blocks: [moment().format('YYYYMMDD')]
+        redirect_to_blocks: [moment().format(FORMAT)]
     })
 })
 
-app.listen(PORT, () => console.log(`Server start on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Server start on port: ${PORT} with format: ${FORMAT}`))
