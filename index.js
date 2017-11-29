23c7d8cb04a9366c90cc9b7e841050b19f2784d4
Krister Johansson <krister.johansson86@outlook.com>
2017-11-29 01:11:10 +0100

    locked

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 const FORMAT = process.env.FORMAT || 'YYYYMMDD'
+const OPENTIME = process.env.OPENTIME || 9
+const LOCKED_BLOCK = process.env.LOCKED_BLOCK || 'YYYYMMDD'
 
 app.get('/', (req, res) => {
-    res.json({
-        redirect_to_blocks: [moment().format(FORMAT)]
-    })
+    if (parseInt(moment().format('hh')) >= OPENTIME) {
+        res.json({
+            redirect_to_blocks: [moment().format(FORMAT)]
+        })
+    } else {
+        res.json({
+            redirect_to_blocks: [LOCKED_BLOCK]
+        })
+    }
 })
 
 app.listen(PORT, () => console.log(`Server start on port: ${PORT} with format: ${FORMAT}`))
